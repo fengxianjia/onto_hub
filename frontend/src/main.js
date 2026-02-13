99b8d2b73e6e64652255241bd42fe0f98863ed66
fengxianjia <fengxianjia@gmail.com>
2026-02-13 11:07:02 +0800

    修复微前端集成后url错误问题

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
 import './style.css'
+import axios from 'axios'
 import App from './App.vue'
+
+// Set base URL for axios
+axios.defaults.baseURL = '/onto_hub'
 
 const app = createApp(App)
 
